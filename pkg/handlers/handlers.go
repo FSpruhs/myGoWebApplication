fb94f8c79c12392d09aeb86f368ab3a0d1978af5
spruhs <fabian@spruhs.com>
2024-12-24 00:17:57 +0100

    templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -33,8 +33,29 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	render.Template(w, "about-page.gohtml", &models.TemplateData{
-		StringMap: map[string]string{"remote_ip": remoteIP},
-	})
+	render.Template(w, "about-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "contact-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) Eremite(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "eremite-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) Couple(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "couple-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) Family(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "family-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "check-availability-page.gohtml", &models.TemplateData{})
+}
+
+func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "make-reservation-page.gohtml", &models.TemplateData{})
 }
